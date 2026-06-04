252f7eac0bae268c8777080253efd12b02533a05
gauravPanditr <gauravpandit7856@gmail.com>
2026-06-04 10:14:11 +0530

    created enitiy

---
+++ app/src/main/java/claimservice/entity/ClaimStatus.java
@@ -0,0 +1,8 @@
+package claimservice.entity;
+
+public enum ClaimStatus {
+   SUBMITTED,
+    IN_REVIEW,
+    APPROVED,
+    REJECTED
+}
--- app/src/main/java/claimservice/entity/Cliam.java
+++ app/src/main/java/claimservice/entity/Cliam.java
@@ -1,5 +1,41 @@
 package claimservice.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliam {
-    
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String userId;
+    private String policyId;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private LocalDate incidentDate;
+    private String incidentLocation;
+
+    @Enumerated(EnumType.STRING)
+    private ClaimStatus status;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        status = ClaimStatus.SUBMITTED;
+    }
 }
